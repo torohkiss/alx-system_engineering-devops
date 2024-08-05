a367ac523b419e9f225c81ca70675ea3bc2c15f1
torohkiss <torohkiss@yahoo.com>
2024-08-04 21:07:57 -0700

    task 0d

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,18 +8,21 @@ import sys
 if __name__ == "__main__":
     user_id = sys.argv[1]
 
-    apiurl = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
+    apiurl = "https://jsonplaceholder.typicode.com/users/{}".format(
+        user_id)
 
     response_name = requests.get(apiurl)
 
     user_name = response_name.json()['name']
 
-    apiurl_todos = "https://jsonplaceholder.typicode.com/todos?userId={}".format(user_id)
-    data_todos = requests.get(apiurl_todos)
+    ai = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
+        user_id)
+    data_todos = requests.get(ai)
     data = data_todos.json()
     total_num_tasks = len(data)
 
-    ac = "https://jsonplaceholder.typicode.com/todos?userId={}&completed=true".format(user_id)
+    ac = "https://jsonplaceholder.typicode.com/todos?userId={}"\
+        "&completed=true".format(user_id)
     data_comp = requests.get(ac)
     data_comp_true = data_comp.json()
     num_comp_tasks = len(data_comp_true)
