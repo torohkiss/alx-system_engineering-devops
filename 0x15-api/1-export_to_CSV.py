5fcc2ec301c1a02967d660d869e1a7ac63d2b2a9
torohkiss <torohkiss@yahoo.com>
2024-08-04 22:01:43 -0700

    task 1a

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -7,36 +7,19 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    user_id = sys.argv[1]
+    uid = sys.argv[1]
+    user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(
+        uid)
+    response = requests.get(user_url)
+    username = response.json().get('username')
+    todo_url = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(
+        uid)
+    todo = requests.get(todo_url).json()
 
-    apiurl = "https://jsonplaceholder.typicode.com/users/{}".format(
-        user_id)
+    with open('{}.csv'.format(uid), 'w', encoding='UTF8', newline='') as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
 
-    response_name = requests.get(apiurl)
-
-    user_name = response_name.json()['name']
-
-    ai = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
-        user_id)
-    data_todos = requests.get(ai)
-    data = data_todos.json()
-    total_num_tasks = len(data)
-
-    ac = "https://jsonplaceholder.typicode.com/todos?userId={}"\
-        "&completed=true".format(user_id)
-    data_comp = requests.get(ac)
-    data_comp_true = data_comp.json()
-    num_comp_tasks = len(data_comp_true)
-
-    #print("Employee {} is done with tasks({}/{}):".format(
-    #    user_name, num_comp_tasks, total_num_tasks))
-
-    comp_task_titles = [task['title'] for task in data_comp_true]
-    #for title in comp_task_titles:
-    #    print('\t {}'.format(title))
-
-    with open('USER_ID.csv', 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(["USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"])
-        for task in data:
-            writer.writerow([user_id, user_name, task['completed'], task['title']])
+        for task in todo:
+            task_status = task.get('completed')
+            task_title = task.get('title')
+            writer.writerow([uid, username, task_status, task_title])
