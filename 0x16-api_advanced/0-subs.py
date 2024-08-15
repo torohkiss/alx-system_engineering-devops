bfadd032c56c6083b2b29bbfc00a54d392a0213f
torohkiss <torohkiss@yahoo.com>
2024-08-15 03:41:38 -0700

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,14 +3,13 @@
 import requests
 
 def number_of_subscribers(subreddit):
-    u_agent = {'User-Agent': '/u/Suspicious-Jelly920'}
-    url = 'https://api.reddit.com/r/{}/about/'.format(subreddit)
-    #url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    r = requests.get(url,  headers=u_agent)
-
+    #url = f"https://api.reddit.com/r/{subreddit}/about/"
+    url = "https://api.reddit.com/r/{}/about/".format(subreddit)
+    uagent = {'User-agent': '0-subs'}
+    r = requests.get(url, headers=uagent, allow_redirects=False)
     if r.status_code == 200:
         data = r.json()
-        total_subscribers = data['data']['subscribers']
+        subs = data['data']['subscribers']
+        return subs
     else:
-        total_subscribers = 0
-    return total_subscribers
+        return 0
