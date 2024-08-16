a37a7077c4f0f3f9e54d5e655951346cedc382cf
torohkiss <torohkiss@yahoo.com>
2024-08-16 02:46:19 -0700

    task 1a

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
-
+"""
+    This module contains a function that queries the
+    Reddit API and prints the top 10 hottest post for
+    a given subreddit.
+"""
 import requests
 
 
 def top_ten(subreddit):
-    url = "https://api.reddit.com/r/{}/hot?limit=10".format(subreddit)
+    """
+        Prints Top ten hottest posts of a subreddit
+        or None
+    """
     u_agent = {'User-Agent': '/u/Suspicious-Jelly920'}
-
-    r = requests.get(url, headers=u_agent)
-
-    if r.status_code == 200:
-        data = r.json()
-        posts = data.get('data', {}).get('children', [])
-        titles = [post['data']['title'] for post in posts]
-        print(titles)
-    else:
-        print(None)
-        """
-        hottest_ten = r.json()["data"]["children"]
+    url = 'https://api.reddit.com/r/{}/hot/'.format(subreddit)
+    res = requests.get(url,  headers=u_agent)
+    if res.status_code == 200:
+        hottest_ten = res.json()["data"]["children"]
 
         count = 0
         for hot in hottest_ten:
@@ -26,4 +25,4 @@ def top_ten(subreddit):
             print(hot["data"]["title"])
             count += 1
     else:
-        print("None")"""
+        print("None")
